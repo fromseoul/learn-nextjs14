d725301ab37bce7f3b57c5fcdb9fd41feff86218
jungmin <jungmin@sovoro.kr>
2024-07-28 14:08:13 +0900

    fix():add constants.ts

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
-import { API_URL } from "../app/(home)/page";
 import styles from "../styles/movie-videos.module.css";
+import { API_URL } from "../app/constants";
 
 
 async function getVideo(id: string) {
