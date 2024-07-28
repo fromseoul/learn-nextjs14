e1d9803f852b74bf5b74eea8b9cae368f66da8fb
jungmin <jungmin@sovoro.kr>
2024-07-28 14:04:08 +0900

    fix():upper case

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import styles from "../styles/Movie.module.css";
+import styles from "../styles/movie.module.css";
 import { useRouter } from "next/navigation";
 
 interface IMovieProps {
